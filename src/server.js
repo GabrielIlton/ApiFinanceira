29cfcff810a8496f1f61b07c72cde6e71b1f0a68
Gabriel Ilton <gabriel.ilton@liveonsolutions.com>
2021-11-09 11:35:06 -0300

    Remove coments

--- src/server.js
+++ src/server.js
@@ -20,31 +20,3 @@ app.use(routes);
 app.listen(3333,() => {
     console.log("Rodando")
 });
-
-// -GET: Leitura
-// -POST: Criação
-// -PULL: Atualização
-// -DELETE: Deleção
-// -PATCH: Atualização parcial
-
-// -1XX: Informação 
-// -2XX: Confirmação
-//     °200 - Requisição bem sucedidda
-//     °201 - Created - Geralmente usado para POST após uma inserção
-
-// -3XX: Redirecionamento
-//     °301 - Moved Permanently
-//     °302 - Moved
-
-// -4XX: Error do client
-//     °400 - Bad request
-//     °401 - Unauthorized
-//     °403 - Forbidden
-//     °404 - Not Found
-//     °422 - Unprocessable Entity
-
-// -5XX: Error no Server
-//     °500 - Internal Server Error
-//     °502 - Bad Gateway
-
-// //todo: splice altera o conteudo, add conteudo novo e remove o velho
